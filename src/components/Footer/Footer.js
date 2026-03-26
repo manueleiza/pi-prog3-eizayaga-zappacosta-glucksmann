ae4f4f7ba48b42601cf1bb1de76e7e1af46bff29
Simongluck <sglucksmann@udesa.edu.ar>
2026-03-26 16:57:48 -0300

    Cambios en el FooteryHeader

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import React from "react";
+
+
+function Footer() {
+    return (
+        <footer>
+            <h3>
+                Integrantes: Manuel Eiyazaga, Simón Glücksmann, Manuel Zappacosta.
+            </h3>
+            <p>
+                Todos los derechos reservados Copyright. © 2026
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,3 @@
+import { Component } from "react";
+import React from "react";
+
