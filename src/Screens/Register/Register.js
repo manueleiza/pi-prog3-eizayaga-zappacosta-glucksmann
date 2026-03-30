c10f67a9d89163c22b88ea771acb777348fcd70e
manueleiza <meizayaga@udesa.edu.ar>
2026-03-30 17:44:15 -0300

    Empezé la validacion del register, falta terminar

--- src/Screens/Register/Register.js
+++ src/Screens/Register/Register.js
@@ -3,25 +3,51 @@ import react, {Component} from "react";
 class Register extends Component{
     constructor(props){
         super(props);
-        this.state = {valor: ''};
+        this.state = {email: '', contraseña: '',
+        };
     }
 
     evitarSubmit(event){
         event.preventDefault();
     } 
     
-    controlarCambios(event){
-        this.setState({valor: event.target.value})
+    controlarEmail(event){
+        this.setState({
+            email: event.target.value
+        
+        });
+    }
+       
+    controlarContraseña(event){
+        this.setState({
+
+            contraseña: event.target.value});
+            console.log(event.target.value)
+        }
+
+    validarContraseña(event){
+        this.setState({
+            contraseña: event.target.value            
+        })
+
     }
 
     render(){
         return(
 
-            <form onSubmit={(event) => this.evitarSubmit(event)}>
+            <form className="form-register" onSubmit={(event) => this.evitarSubmit(event)}>
 
-                <label>Nombre: </label>
-                <input type="text" onChange={(onChange) => this.controlarCambios(Event)} value={this.state.valor}/>
-                <input type="submit" value="Submit"/>
+                <label>Correo Electronico: </label>
+                <input type="text" onChange={(event) => this.controlarEmail(event)} value={this.state.valor}/>
+               
+                <label>Constrsaeña: </label>
+                <input type="text" onChange={(event) => this.controlarContraseña(event)} value={this.state.valor}/>
+            
+                <input className="boton" type="submit" value="Submit"/>
+
+
+
+
             </form>
 
 
