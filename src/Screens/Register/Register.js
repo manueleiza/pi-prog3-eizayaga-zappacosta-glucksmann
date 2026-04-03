a3d83997c0597d91b443f2ad57e558f314d72732
manueleiza <meizayaga@udesa.edu.ar>
2026-04-03 20:19:23 -0300

    Avancé bastante con el formulario de registro

--- src/Screens/Register/Register.js
+++ src/Screens/Register/Register.js
@@ -1,58 +1,10 @@
-import react, {Component} from "react";
+import react from "react";
+import FormRegister from "../../components/FormRegister/FormRegister";
 
-class Register extends Component{
-    constructor(props){
-        super(props);
-        this.state = {email: '', contraseña: '',
-        };
-    }
-
-    evitarSubmit(event){
-        event.preventDefault();
-    } 
-    
-    controlarEmail(event){
-        this.setState({
-            email: event.target.value
-        
-        });
-    }
-       
-    controlarContraseña(event){
-        this.setState({
-
-            contraseña: event.target.value});
-            console.log(event.target.value)
-        }
-
-    validarContraseña(event){
-        this.setState({
-            contraseña: event.target.value            
-        })
-
-    }
-
-    render(){
-        return(
-
-            <form className="form-register" onSubmit={(event) => this.evitarSubmit(event)}>
-
-                <label>Correo Electronico: </label>
-                <input type="text" onChange={(event) => this.controlarEmail(event)} value={this.state.valor}/>
-               
-                <label>Constrsaeña: </label>
-                <input type="text" onChange={(event) => this.controlarContraseña(event)} value={this.state.valor}/>
-            
-                <input className="boton" type="submit" value="Submit"/>
-
-
-
-
-            </form>
-
-
-        )
-    }
+function Register(){
+    return(
+        <FormRegister/>
+    )
 }
 
 export default Register
---
+++ src/components/FormRegister/FormRegister.js
@@ -0,0 +1,110 @@
+import react, { Component } from "react";
+import "./register.css"
+
+class FormRegister extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            correo: '',
+            contraseña: '',
+            enviado: false,
+            validarCorreo: false,
+
+        };
+    }
+
+    evitarSubmit(event) {
+        event.preventDefault();
+        this.setState({
+            enviado: true,
+        })
+
+        if (this.state.contraseña.length >= 6) {
+
+            const usuario = {
+                correo: this.state.correo,
+                contraseña: this.state.contraseña
+            }
+
+            let correoUsado = localStorage.getItem("correoUsuario");
+
+            if (this.state.correo === correoUsado) {
+                this.setState({
+                    validarCorreo: true,
+                })
+
+                console.log("correo usado")
+                return;
+            }
+
+            localStorage.setItem("correoUsuario", this.state.correo);
+            localStorage.setItem("constrseñaUsuario", this.state.contraseña)
+            console.log(localStorage)
+
+
+        }
+
+        console.log("el formulario se envió")
+    }
+
+    crearContraseña(event) {
+        this.setState({
+
+            contraseña: event.target.value
+        })
+
+        console.log("contraseña:" + this.state.contraseña)
+    }
+
+    crearCorreo(event) {
+        this.setState({
+
+            correo: event.target.value,
+        })
+
+        console.log("correo:" + this.state.correo)
+    }
+
+    validarCorreo(event) {
+
+
+    }
+
+    validarContraseña(event) {
+
+    }
+
+
+
+
+
+
+    render() {
+        return (
+
+            <form className="form-register" onSubmit={(event) => this.evitarSubmit(event)}>
+
+                <label>Correo Electronico: </label>
+                <input type="text" onChange={(event) => this.crearCorreo(event)} value={this.state.correo} />
+                {this.state.correo.length === 0 && this.state.enviado ? <p className="error-contraseña">El campo está vacio</p> : ""}
+                {this.state.validarCorreo ? <p className="error-contraseña">Este correo tiene una cuenta asociada existente</p> : ""}
+
+                <label>Constrsaeña: </label>
+                <input type="text" onChange={(event) => this.crearContraseña(event)} value={this.state.contraseña} />
+                {this.state.contraseña.length === 0 && this.state.enviado ? <p className="error-contraseña">El campo está vacio</p> : ""}
+
+                {this.state.contraseña.length !=0 && this.state.contraseña.length < 6 && this.state.enviado ? <p className="error-contraseña">La contraseña debe contener al menos 6 caracteres</p> : ""}
+
+                <input className="boton" type="submit" value="Submit" />
+
+
+
+
+            </form>
+
+
+        )
+    }
+}
+
+export default FormRegister
