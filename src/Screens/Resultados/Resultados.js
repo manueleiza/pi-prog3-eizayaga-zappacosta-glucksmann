102a2962efbff1e180ffb9619a3d9aa234fc0cd0
manueleiza <meizayaga@udesa.edu.ar>
2026-04-16 15:09:46 -0300

    termine el buscador y el resultados de busqueda

--- src/Screens/Resultados/Resultados.js
+++ src/Screens/Resultados/Resultados.js
@@ -1,13 +1,59 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import Card from "../../components/Card/Card";
+import "../../components/SeccionPelis/SeccionPelis.css"
 
-export default class Resultados extends Component {
-    constructor(props){
-        super(props)
+class Resultados extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            resultados: [],
+            cargando: true,
+        };
     }
 
-    render(){
-        return(
-            <div>Resultados de: {this.props.match.params.busqueda}</div>
-        )
+    componentDidMount() {
+        const busquedaUsuario = this.props.match.params.busqueda;
+
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=eaa57596af1d15ddb4b8b1c407e61403&query=${busquedaUsuario}`).then(res => res.json())
+            .then(data => this.setState({
+                resultados: data.results,
+                cargando: false
+            }))
+            .catch(error => console.log(error));
+    }
+
+    filtrarPelicula() {
+
+    }
+
+
+    render() {
+        return (
+            <main>
+                <h4>Resultados de: {this.props.match.params.busqueda}</h4>
+
+                {this.state.cargando ? (
+                    <p>Cargando...</p>
+                ) : (
+                    <section className="row-cards">
+                        {this.state.resultados != null && this.state.resultados.length > 0 ? (
+                            this.state.resultados.map(pelicula => (
+                                <Card className="pelicula"
+                                    key={pelicula.id}
+                                    id={pelicula.id}
+                                    title={pelicula.original_title}
+                                    image={pelicula.poster_path}
+                                    description={pelicula.overview}
+                                />
+                            ))
+                        ) : (
+                            <p>No se encontraron resultados.</p>
+                        )}
+                    </section>
+                )}
+            </main>
+        );
     }
 }
+
+export default Resultados
--- src/components/Buscador/Buscador.js
+++ src/components/Buscador/Buscador.js
@@ -11,20 +11,24 @@ class Buscador extends Component {
     onSumbit(event) {
         event.preventDefault()
         console.log("props de buscador", this.props)
-        this.props.history.push("/busqueda/" + this.state.search)
+        this.props.history.push(`/busqueda/${this.state.search}`)
     }
 
     guardarBusqueda(event) {
-        this.setState({ search: event.target.value }, () => console.log("log desde el setState extendido:", this.state.search))
+        this.setState({ 
+        
+        search: event.target.value }, () => console.log("log desde el setState extendido:", this.state.search))
         console.log("El valor en estado es:", this.state.search)
     }
+
+
 
     render() {
         return (
             <div>
                 <form onSubmit={(event) => this.onSumbit(event)}>
-                    <input onChange={(event) => this.guardarBusqueda(event)} value={this.state.search}></input>
-                    <button type="sumbit">Buscar</button>
+                    <input type="text" placeholder="Buscar Peliculas o Series" onChange={(event) => this.guardarBusqueda(event)} value={this.state.search}></input>
+                    <button type="submit">Buscar</button>
                     
                 </form>
             </div>
